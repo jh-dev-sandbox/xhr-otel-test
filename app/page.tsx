f239d3f49699364e967b6b7d3d07e542d359c09d
Jozef Harag <jh.developer@pm.me>
2025-05-27 13:58:08 +0200

    chore: remove test header

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
   const callApi = (version: "v1" | "v2") => {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", `/api/${version}/json`);
-    xhr.setRequestHeader("traceparent", "test-traceparent-value");
     xhr.onload = function () {
       if (xhr.status === 200) {
         const json = JSON.parse(xhr.responseText);
